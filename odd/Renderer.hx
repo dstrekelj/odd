417c2fa8f1459d5b5959cd4590c9096ea8af6a25
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2015-12-20 10:37:16 +0100

    Updated Renderer class and build file.

--- odd/Renderer.hx
+++ odd/Renderer.hx
@@ -1,8 +1,9 @@
 package odd;
 import odd.renderers.js.CanvasRenderer;
 import odd.renderers.js.OddCanvasRenderer;
-/**
- * @author 
- */
 
+#if (js && ODD_CANVAS_RENDERER)
+typedef Renderer = OddCanvasRenderer;
+#else
 typedef Renderer = CanvasRenderer;
+#end

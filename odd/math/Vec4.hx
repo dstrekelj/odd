dc356ef9b7cfdb426210e68894860cb8184b50c2
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2015-12-22 10:28:10 +0100

    Updated Vec4, added division by scalar.

--- odd/math/Vec4.hx
+++ odd/math/Vec4.hx
@@ -7,7 +7,7 @@ class Vec4
     public var z : Float;
     public var w : Float;
 
-    public function new(x : Float = 0, y : Float = 0, z : Float = 0, w : Float = 1)
+    public function new(?x : Float = 0, ?y : Float = 0, ?z : Float = 0, ?w : Float = 1)
     {
         this.x = x;
         this.y = y;
@@ -28,45 +28,87 @@ abstract Vector4(Vec4) from Vec4 to Vec4
 
     inline function get_length() : Float
     {
-        return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z + this.w * this.w);
+        return Math.sqrt(dotProduct(this));
     }
     
-    inline public function new(x : Float, y : Float, z : Float, w : Float)
+    inline public function new(?x : Float, ?y : Float, ?z : Float, ?w : Float)
     {
-        this = new Vec4(x, y, z, w);
+        this = new Vec4(
+            x,
+            y,
+            z,
+            w
+        );
     }
 
     @:op(-A)
     inline function negate() : Vector4
     {
-        return new Vector4(-this.x, -this.y, -this.z, -this.w);
+        return new Vector4(
+            -this.x,
+            -this.y,
+            -this.z,
+            -this.w
+        );
     }
 
     @:op(A + B)
-    @:commutative
     inline function add(B : Vector4) : Vector4
     {
-        return new Vector4(this.x + B.x, this.y + B.y, this.z + B.z, this.w + B.w);
+        return new Vector4(
+            this.x + B.x,
+            this.y + B.y,
+            this.z + B.z,
+            this.w + B.w
+        );
     }
 
     @:op(A - B)
-    @:commutative
     inline function subtract(B : Vector4) : Vector4
     {
-        return new Vector4(this.x - B.x, this.y - B.y, this.z - B.z, this.w - B.w);
+        return new Vector4(
+            this.x - B.x,
+            this.y - B.y,
+            this.z - B.z,
+            this.w - B.w
+        );
     }
 
     @:op(A * B)
-    @:commutative
-    inline function scalarProduct(B : Float) : Vector4
+    inline function multiply(B : Float) : Vector4
     {
-        return new Vector4(this.x * B, this.y * B, this.z * B, this.w * B);
+        return new Vector4(
+            this.x * B,
+            this.y * B,
+            this.z * B,
+            this.w * B
+        );
+    }
+    
+    @:op(A / B)
+    inline function divide(B : Float) : Vector4
+    {
+        return new Vector4(
+            this.x / B,
+            this.y / B,
+            this.z / B,
+            this.w / B
+        );
     }
 
     @:op(A * B)
-    @:commutative
     inline function dotProduct(B : Vector4) : Float
     {
         return this.x * B.x + this.y * B.y + this.z * B.z + this.w * B.w;
+    }
+    
+    public inline function normalize() : Vector4
+    {
+        return new Vector4(
+            this.x / length,
+            this.y / length,
+            this.z / length,
+            this.w / length
+        );
     }
 }

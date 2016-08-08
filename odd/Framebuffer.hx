d9c3b311f1ace3a11f3e1f6e07e0213144e669d3
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2016-08-08 20:23:47 +0200

    Make framebuffer an interface
    The render targets implement the interface for their specific framebuffer
    implementation. The context provides the user with a reference to the
    interface instead of the implementation.

--- odd/Framebuffer.hx
+++ odd/Framebuffer.hx
@@ -1,34 +1,19 @@
 package odd;
 
 /**
- * Stub.
- * 
- * The framebuffer should allow per-pixel and per-channel operations.
- * 
- * TODO: Have `Framebuffer` serve as an abstraction layer over
- * target-specific implementation.
+ * The framebuffer allows per-pixel and per-channel operations.
  */
-class Framebuffer 
+interface Framebuffer 
 {
     public var width(default, null) : Int;
     public var height(default, null) : Int;
-    public var data(default, null) : Array<Int>;
-    
-    public function new(width : Int, height : Int)
-    {
-        this.width = width;
-        this.height = height;
-    }
-    
-    public function clear(rgb : Int) : Void {}
-    
-    public function getPixel(x : Int, y : Int) : Int { return -1; }
-    public function getR(x : Int, y : Int) : Int { return -1; }
-    public function getG(x : Int, y : Int) : Int { return -1; }
-    public function getB(x : Int, y : Int) : Int { return -1; }
-    
-    public function setPixel(x : Int, y : Int, rgb : Int) : Void {}
-    public function setR(x : Int, y : Int, r : Int) : Void {}
-    public function setG(x : Int, y : Int, g : Int) : Void {}
-    public function setB(x : Int, y : Int, b : Int) : Void {}
+    public function clear(rgb : Int) : Void;
+    public function getPixel(x : Int, y : Int) : Int;
+    public function getR(x : Int, y : Int) : Int;
+    public function getG(x : Int, y : Int) : Int;
+    public function getB(x : Int, y : Int) : Int;
+    public function setPixel(x : Int, y : Int, rgb : Int) : Void;
+    public function setR(x : Int, y : Int, r : Int) : Void;
+    public function setG(x : Int, y : Int, g : Int) : Void;
+    public function setB(x : Int, y : Int, b : Int) : Void;
 }
